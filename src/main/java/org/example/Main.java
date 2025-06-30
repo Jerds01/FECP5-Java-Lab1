b99e520f8fbe7a62278481a6427780a2e406995b
jerome.yang <jerome.yang@gcash.com>
2025-06-30 14:49:09 +0800

    Working Code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,21 @@
 package org.example;
-
+import java.util.Scanner;
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static double convertToFarenheight(float celcius){
+//        double celcius = celcius;
+        double farenheight = (celcius * 9/5) + 32;
+        return farenheight;
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter temperature in Celsius: ");
+        float temp = scanner.nextFloat();
+        System.out.printf("Temperature in Fahrenheit: %.2f",convertToFarenheight(temp));
+
+
 
     }
 }
